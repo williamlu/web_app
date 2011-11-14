7c431ce295d3aaf50b346840c28500cae72c9c56
warpblade <william.lu10@gmail.com>
2011-11-13 19:09:08 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebApp::Application.config.secret_token = '054044403b8d8524e157e4e74eaab58bdf936f16d615956cbe946afa915fb945abb862ef3f790e386da51eb8218862d8557dcd83db5b2260f7143c3cbf57b671'
